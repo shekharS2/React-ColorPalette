a23b34045a604a304bb852e37d105868f4fbb5fa
shekharS2 <saikiashekhar25@gmail.com>
2021-11-22 20:23:48 +0530

    first commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,89 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+
+function OutputColor(props){
+  let divStyle = {
+    'background' : props.choosenColor
+  };
+  return (
+    <div class ="output" style = {divStyle}>
+      <p>{
+        props.choosenColor == "Color not chosen" ? props.choosenColor : ""
+      }</p>
+    </div>
+  );
+}
+
+
+function InputColor(props){
+    let buttonStyle = {
+      'background-color' : props.color
+    };
+    return (
+        <div>
+          <button style={buttonStyle}
+            onClick={props.chooseColor}
+          >
+          </button>
+        </div>
+    );
+}
+
+function InputPalette(props){
+  
+  
+  return (
+      <div class="flex-container">
+        {
+          props.colors.map((color) => {
+            return (
+              <>
+                <InputColor
+                  color = {color}
+                  chooseColor = {() => {props.setColor(color)}}
+                />
+              </>
+            );
+          })
+        }
+      </div>
+      
+  );
+}
+
+
+function ColorApp() {
+  
+  const [colors, setColors] = useState(['Black', 'Red', 'Green', 'Brown', 'Yellow', 'Blue', 'Orange', 'Purple', 'Pink']);
+  let [color, setColor] = useState('Color not chosen');
+
+  
+  return (
+    <div>
+      <div class="flex-container">
+        <InputPalette
+          colors = {colors}
+          setColor = {
+            (choosenColor) => {
+              setColor(choosenColor);
+              console.log(color);
+            }
+          }
+        />
+      </div>
+      <OutputColor choosenColor = {color}/>
+      
+    </div>
+  );
+}
+
+
+
+
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <ColorApp />,
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
